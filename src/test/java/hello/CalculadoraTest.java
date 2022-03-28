36b45f30e9db6c2e8f388b17f0a529634e2b9c0f
AlbertoFreije <freicartobe@gmail.com>
2022-03-28 10:17:46 +0200

    Pruebas finales

--- src/test/java/hello/CalculadoraTest.java
+++ src/test/java/hello/CalculadoraTest.java
@@ -1,17 +1,33 @@
 package hello;
 
-import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+
+import hello.Calculadora;
 
 public class CalculadoraTest {
   
   private Calculadora calculadora = new Calculadora(4,2);
 
   @Test
-  public void calculadoraSumarTest2() {
+  public void calculadoraSumarTest() {
     assertTrue(calculadora.sumar() == 6);
+  }
+  
+  @Test
+  public void calculadoraRestaTest() {
+    assertTrue(calculadora.restar() == 2);
+  }
+  
+  @Test
+  public void calculadoraMultiplicarTest() {
+    assertTrue(calculadora.multiplicar() == 8);
+  }
+  
+  @Test
+  public void calculadoraDividirTest() {
+    assertTrue(calculadora.dividir() == 2);
   }
 
 }
