5e9e53ad4b58552abff0a9b5234622eab1ee6148
AlbertoFreije <freicartobe@gmail.com>
2022-03-28 10:23:39 +0200

    deberian ir todos los test

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,7 +1,18 @@
 package hello;
 
-public class Greeter {
-  public String sayHello() {
-    return "Hello world!";
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import hello.Greeter;
+
+public class GreeterTest {
+  
+  private Greeter greeter = new Greeter();
+
+  @Test
+  public void greeterSaysHello() {
+    assertEquals("Hello world!", greeter.sayHello());
   }
+
 }
