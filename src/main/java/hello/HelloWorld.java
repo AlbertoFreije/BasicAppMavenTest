dfc40c1e438564ba14a6285b29359a6374cf4b88
AlbertoFreije <freicartobe@gmail.com>
2022-03-29 09:11:20 +0200

    prueba

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -9,15 +9,15 @@ public class HelloWorld {
         return "Hello World!, I'm "+name;
     }
 
-  public static void main(String[] args) {
-    LocalTime currentTime = new LocalTime();
-    System.out.println("The current local time is: " + currentTime);
-    Greeter greeter = new Greeter();
-    Calculadora calculadora = new Calculadora(4,2);
-    System.out.println("Prueba de impresion");
-    System.out.println(calculadora.sumar()+"");
-    System.out.println(calculadora.restar()+"");
-    System.out.println(greeter.sayHello());
-    System.out.println(greeting("Juan Pablo"));
-  }
+  // public static void main(String[] args) {
+  //   LocalTime currentTime = new LocalTime();
+  //   System.out.println("The current local time is: " + currentTime);
+  //   Greeter greeter = new Greeter();
+  //   Calculadora calculadora = new Calculadora(4,2);
+  //   System.out.println("Prueba de impresion");
+  //   System.out.println(calculadora.sumar()+"");
+  //   System.out.println(calculadora.restar()+"");
+  //   System.out.println(greeter.sayHello());
+  //   System.out.println(greeting("Juan Pablo"));
+  // }
 }
